1345feec5415794106df9326117aa9bddda6cfd3
lucioacri <lucioacri@gmail.com>
2025-06-04 14:27:51 +0200

    add posts context

---
+++ contexts/PostContext.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const PostsContext = createContext();
+
+function PostsProvider({ children }) {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/bacheca")
+      .then((response) => {
+        setPosts(response.data.Object);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <PostsContext.Provider value={posts}>{children}</PostsContext.Provider>
+  );
+}
+
+function usePosts() {
+  const context = useContext(PostsContext);
+  return context;
+}
+
+export { PostsProvider, usePosts };
